7a6c889c001b4574c0a74f94918eff518dcc0fe4
prashant-g0 <mail.prashantg0@gmail.com>
2025-11-04 16:45:12 +0530

    leetcode 51

--- Backtracking/nQueens.java
+++ Backtracking/nQueens.java
@@ -10,7 +10,7 @@
 // Input: n = 1
 // Output: [["Q"]]
 
-
+import java.util.Scanner;
 class nQueens {
 
     public static boolean isSafe(char[][] board, int row, int col){
@@ -54,6 +54,25 @@ class nQueens {
         }
     }
 
+    public static boolean oneWay(char[][] board, int row){
+        int totCol = board.length;
+        if(row == totCol){
+            return true;
+        }
+
+        for(int j=0; j<totCol; j++){
+            if(isSafe(board, row, j)){ // place queen only when safe to place
+                board[row][j] = 'Q';
+                if(oneWay(board, row+1)){ // recursion
+                    return true;
+                } 
+                board[row][j] = '.'; // backtracking
+            }
+        }
+
+        return false;
+    }
+
     public static void printBoard(char[][] board){
         System.err.println("======Board======");
         int n = board.length;
@@ -65,20 +84,60 @@ class nQueens {
         }
     }
 
-    static int count = 0;
-    public static void main(String[] args) {
-        int n = 4;
-        char[][] board = new char[n][n];
-
+    public static void initializeBoard(char[][] board){
+        int n = board.length;
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 board[i][j] = '.';
             }
         }
+    }
 
-        placeQueens(board, 0);
-        System.out.println("Total Solutions: " + count);
-        //printBoard(board);
+    static int count = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter board size (n): ");
+        int n = sc.nextInt();
+
+        while (true) {
+            System.out.println("Enter \n1. Print all solutions (if exists). \n2. Print only one solution (if exists). \n3. Print total number of solutions. \n4. Exit.");
+            int choice = sc.nextInt();
+            count = 0;
+
+            switch (choice) {
+                case 1:
+                    char[][] board1 = new char[n][n];
+                    initializeBoard(board1);
+                    placeQueens(board1, 0);
+                    break;
+
+                case 2:
+                    char[][] board2 = new char[n][n];
+                    initializeBoard(board2);
+                    if (oneWay(board2, 0)) {
+                        printBoard(board2);
+                    } else {
+                        System.out.println("No solution exists.");
+                    }
+                    break;
+
+                case 3:
+                    char[][] board3 = new char[n][n];
+                    initializeBoard(board3);
+                    placeQueens(board3, 0);
+                    System.out.println("Total Solutions: " + count);
+                    break;
+
+                case 4:
+                    System.out.println("Exiting...");
+                    sc.close();
+                    return;
+
+                default:
+                    System.out.println("Invalid! Try again");
+            }
+        }
     }
 }
 
