745c066f1a11b5edff8574b4abeeafac24faa9bd
prashant-g0 <mail.prashantg0@gmail.com>
2025-11-23 20:03:32 +0530

    stack using linkedlist

---
+++ Stack/UsingLinkedList.java
@@ -0,0 +1,52 @@
+public class UsingLinkedList {
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static class Stack{
+        Node head = null;
+
+        public boolean isEmpty(){
+            return head == null;
+        }
+
+        public void push(int data){
+            Node newNode = new Node(data);
+
+            if(isEmpty()){
+                head = newNode;
+                return;
+            }
+
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public int pop(){
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        public int peek(){
+            return head.data;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
