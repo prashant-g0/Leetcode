bd562b48b4d89a0ce361cc85921b69992c78b7ea
prashant-g0 <mail.prashantg0@gmail.com>
2025-12-02 09:00:39 +0530

    gfg - interleaving queue halves

---
+++ Queue/InterleaveQueues.java
@@ -0,0 +1,26 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class InterleaveQueues {
+    public static void interleave(Queue<Integer> q){
+        Queue<Integer> temp = new LinkedList<>();
+        int size = q.size();
+        
+        for(int i=0; i<size/2; i++){
+            temp.add(q.remove());
+        }
+
+        for(int i=0; i<size/2; i++){
+            q.add(temp.remove());
+            q.add(q.remove());
+        }
+    }
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=1; i<=10; i++){
+            q.add(i);
+        }
+        interleave(q);
+        while(!q.isEmpty()) System.out.println(q.remove());
+    }
+}
