17a642adfb1f79c8725b4e13281db6507925918d
prashant-g0 <mail.prashantg0@gmail.com>
2026-01-24 11:21:02 +0530

    added : code

--- Greedy/IndianCoins.java
+++ Greedy/IndianCoins.java
@@ -9,5 +9,25 @@
 import java.util.*;
 
 public class IndianCoins {
-    
+    public static void main(String[] args) {
+        int coins[] = {1, 2, 5, 10, 20, 50, 100, 200, 500};
+        int count = 0;
+        int amount = 2220;
+        ArrayList<Integer> req_coins = new ArrayList<>();
+
+        for (int i=coins.length-1; i>=0; i--){
+                while(amount >= coins[i]){
+                    count++;
+                    req_coins.add(coins[i]);
+                    amount -= coins[i];
+                }
+        }
+
+        System.out.println("Minimum coins required: "+count);
+        System.out.print("Required Coins: ");
+        for(int i=0; i<req_coins.size(); i++){
+            System.out.print(req_coins.get(i)+" ");
+        }
+        System.out.println();
+    }
 }
