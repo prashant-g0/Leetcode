d513bd350da5f8b209ebbaad2f0036ff3c2fa3aa
prashant-g0 <mail.prashantg0@gmail.com>
2026-01-24 11:01:25 +0530

    Indian Coins

---
+++ Greedy/IndianCoins.java
@@ -0,0 +1,13 @@
+// Problem Statement:
+// We are given an infinite supply of denominations [1, 2, 5, 10, 20, 50, 100, 500, 2000]
+// Find min no. of coins/notes to make change for a value V.
+
+// Example:
+// Input: V = 121
+// Output: ans = 3 (100+20+1)
+
+import java.util.*;
+
+public class IndianCoins {
+    
+}
