c11fb9fc36c13d6b406213d17b9e26ebe1829be2
prashant-g0 <mail.prashantg0@gmail.com>
2026-01-24 11:27:07 +0530

    JOb Sequencing Problem

---
+++ Greedy/JobSequence.java
@@ -0,0 +1,13 @@
+// Problem Statement:
+// Given an array of jobs where every job has a deadline and profit if the job is finished before the deadline.
+// It is also given that every job takes a single unit of time so the minimum possible deadline for any job is 1.
+// Maximize the total profit if only job can be scheduled at a time.
+
+// Example:
+// Input: JobA = (4, 20), JobB = (1, 10), JobC = (1, 40), JobD = (1, 30)
+// Output: ans = JobC, JobA
+
+import java.util.*;
+public class JobSequence {
+    
+}
