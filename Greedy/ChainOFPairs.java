45d9aaf719b82f91e9ca933a84a3b01c099f1338
prashant-g0 <mail.prashantg0@gmail.com>
2026-01-24 10:43:45 +0530

    Max length Chain of Pairs

---
+++ Greedy/ChainOFPairs.java
@@ -0,0 +1,13 @@
+// Problem Statement:
+// You're given N pairs of numbers. 
+// In every pair, the first number is always smaller than the 
+// second number. A pair (c,d) can come after pair (a,b) if b<c.
+// Find the longest chain which can be formet from a given set of pairs.
+
+// Example:
+// Input: pairs = (5, 24), (39, 60), (5, 28),  (27, 40), (50, 90)
+// Output: ans = 3
+
+public class ChainOFPairs {
+    
+}
