c61ecee08184041b2c73b2441456cf87e716d54f
prashant-g0 <mail.prashantg0@gmail.com>
2026-01-24 10:55:47 +0530

    added : code

--- Greedy/ChainOFPairs.java
+++ Greedy/ChainOFPairs.java
@@ -8,6 +8,23 @@
 // Input: pairs = (5, 24), (39, 60), (5, 28),  (27, 40), (50, 90)
 // Output: ans = 3
 
+import java.util.*;
 public class ChainOFPairs {
-    
+    public static void main(String[] args) {
+        int pairs[][] = {{5,24}, {39, 60}, {5,28}, {27, 40}, {50, 90}};
+        Arrays.sort(pairs, Comparator.comparingDouble(o -> o[1]));
+
+        int chain_len = 1;
+        int chain_end = pairs[0][1];
+
+        for(int i=1; i<pairs.length; i++){
+            if(pairs[i][0] > chain_end){
+                chain_len++;
+                chain_end = pairs[i][1];
+            }
+        }
+
+        System.out.println("Chain Length: "+chain_len);
+
+    }
 }
