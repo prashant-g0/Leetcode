ca0a94f21275a91c7fa4552ae802adab82333aa2
prashant-g0 <mail.prashantg0@gmail.com>
2026-01-24 18:54:06 +0530

    Chocola Problem

---
+++ Greedy/Chocola.java
@@ -0,0 +1,11 @@
+// We are given a bar of chocolate composed of MxN square pieces.
+// One should break the chocolate into single squares.
+// Each break of a part of the chocolate is charged a cost expressed by a positive integer. This cost does not depend on the size of the part that is being broken but inly on the line the break goes on.
+// Let us denote the costs of breaking along consecutive vertical lines with x1, x2, ..., xM-1 and along horizontal lines with y1, y2, ..., yN-1.
+// Compute the minimal cost of breaking the whole chocolate into single squares.
+
+import java.util.*;
+
+public class Chocola {
+    
+}
