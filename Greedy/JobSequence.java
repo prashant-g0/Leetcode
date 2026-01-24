566c4f03384c804cb6d547cbac2b78bbfb7c6525
prashant-g0 <mail.prashantg0@gmail.com>
2026-01-24 11:49:24 +0530

    added : code

--- Greedy/JobSequence.java
+++ Greedy/JobSequence.java
@@ -9,5 +9,43 @@
 
 import java.util.*;
 public class JobSequence {
-    
+    static class Job{
+        int deadline;
+        int profit;
+        int id;
+
+        public Job(int i, int d, int p){
+            id = i;
+            deadline = d;
+            profit = p;
+        }
+    }
+    public static void main(String[] args) {
+        int job_seq[][] = {{4, 20}, {1, 10}, {1, 40}, {1, 30}};
+
+        ArrayList<Job> jobs = new ArrayList<>();
+        for(int i=0; i<job_seq.length; i++){
+            jobs.add(new Job(i, job_seq[i][0], job_seq[i][1]));
+        }
+
+        Collections.sort(jobs, (a,b) -> b.profit-a.profit);
+
+        ArrayList<Integer> res_seq = new ArrayList<>();
+        int time = 0;
+
+        for(int i=0; i<jobs.size(); i++){
+            Job curr = jobs.get(i);
+            if(curr.deadline > time){
+                res_seq.add(curr.id);
+                time++;
+            }
+        }
+
+        System.out.println("Max Jobs: "+res_seq.size());
+        System.out.print("Jobs Completed: ");
+        for(int i=0; i<res_seq.size(); i++){
+            System.out.print(res_seq.get(i)+" ");
+        }
+        System.out.println();
+    }
 }
