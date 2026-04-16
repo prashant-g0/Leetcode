2a02c4219ae4a1b619b42382badd66f0ab7f490b
prashant-g0 <mail.prashantg0@gmail.com>
2026-04-16 23:59:29 +0530

    v1

---
+++ DAA/bellman-ford.py
@@ -0,0 +1,38 @@
+class Edge:
+    def __init__(self, u, v, w):
+        self.u, self.v, self.w = u, v, w
+    
+def bellman_ford(vertices, edges, source):
+    dist = [float('inf')]*vertices
+    dist[source] = 0
+    
+    for _ in range(vertices-1):
+        for edge in edges:
+            if dist[edge.u] != float('inf') and dist[edge.u] + edge.w < dist[edge.v]:
+                dist[edge.v] = dist[edge.u] + edge.w
+        
+    for edge in edges:
+        if dist[edge.u] != float('inf') and dist[edge.u] + edge.w < dist[edge.v]:
+            print("Graph contains negative cycle")
+            return None
+    
+    return dist
+
+vertices = int(input("Enter number of vertices: "))
+edges_count = int(input("Enter number of edges: "))
+
+edges = []
+for i in range(edges_count):
+    u, v, w = map(int, input("Enter u, v, weight: "). split())
+    edges.append(Edge(u-1, v-1, w))
+    
+source = int(input("Enter source vertex: "))
+distances = bellman_ford(vertices, edges, source)
+
+if distances:
+    print("\nVertex\tDistance from Source")
+    for i in range(vertices):
+        if distances[i] == float('inf'):
+            print(f"{i+1}\tINF")
+        else:
+            print(f"{i+1}\t{distances[i]}")
