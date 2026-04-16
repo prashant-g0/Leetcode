d38de1b67141da52002adc914743609670380ae3
prashant-g0 <mail.prashantg0@gmail.com>
2026-04-16 22:18:40 +0530

    added new method

--- DAA/quicksort.py
+++ DAA/quicksort.py
@@ -1,3 +1,4 @@
+# Easiest method
 def quicksort(arr):
     if len(arr) <= 1: return arr
     
@@ -9,7 +10,30 @@ def quicksort(arr):
     
     return quicksort(left) + middle + quicksort(right)
 
+# ma'am's method
+def partition(arr, l, r, pivot):
+    while l <= r:
+        while arr[l] < pivot: l += 1
+        while arr[r] > pivot: r -= 1
+        
+        if l <= r:
+            arr[l],arr[r] = arr[r],arr[l]
+            l += 1
+            r -= 1
+    return l
+
+def quick_sort(arr, st, end):
+    if st < end:
+        pivot = arr[st+(end-st)//2]
+        idx = partition(arr, st, end, pivot)
+        quick_sort(arr, st, idx-1)
+        quick_sort(arr, idx, end)
+
 if __name__ == "__main__":
     arr = [4, 5, 6, 2, 1, 1, 0, -1, -35, 45]
     print(f"Elements: {arr}")
-    print(f"Sorted: {quicksort(arr)}")
+    print(f"Sorted (Easiest Method): {quicksort(arr)}")
+    
+    print(f"Elements: {arr}")
+    quick_sort(arr, 0, len(arr)-1)
+    print(f"Sorted (Ma'am's Method): {arr}")
